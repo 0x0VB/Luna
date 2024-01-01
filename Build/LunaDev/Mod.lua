190e3324d12cb475c572797a1daef908d5fdf759
uniquadev <uniquadev@gmail.com>
2024-01-01 16:16:18 +0100

    updated .gitignore cache

--- Build/LunaDev/Mod.lua
+++ Build/LunaDev/Mod.lua
@@ -2,10 +2,3 @@
 LawnApp.WindowTitle = "Luna!";
 LawnApp.Speed = 2;
 local Root = LawnApp.UIRoot;
-LawnApp.OnUpdate:Connect(function ()
-    local Children = LawnApp.UIRoot:GetChildren();
-    for _, Element in pairs(Children) do
-        Element.InitPos = Element.InitPos or Element.Position;
-        Color();
-    end;
-end)
---
+++ Build/Mods/crash.lua
@@ -0,0 +1,4 @@
+LawnApp.OnUpdate:Connect(function ()
+    Vector2();
+    collectgarbage("collect");
+end)
