0580f0dbed4102aa2fc08481fe6cab719be3f451
uniquadev <uniquadev@gmail.com>
2024-01-04 20:50:20 +0100

    removed italian comments generated from vs

--- LunaUtil/pch.cpp
+++ LunaUtil/pch.cpp
@@ -1,5 +1,1 @@
-// pch.cpp: file di origine corrispondente all'intestazione precompilata
-
 #include "pch.h"
-
-// Quando si usano intestazioni precompilate, questo file è necessario per la riuscita della compilazione.
