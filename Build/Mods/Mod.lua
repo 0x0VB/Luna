9b2c96a6d2b8879cc873aaf64aa13c6d927be7c3
uniquadev <uniquadev@gmail.com>
2024-01-04 20:49:46 +0100

    reorganized repo solution file to support more projects

--- Build/Mods/Mod.lua
+++ Build/Mods/Mod.lua
@@ -1,6 +1,11 @@
 --// Luna V0.2.2 \\--
-print(Luna)
-print(LawnApp)
-LawnApp.OnUpdate:Connect(function()
-    print(LawnApp.Age)
-end);
+LawnApp.WindowTitle = "Luna!";
+LawnApp.Speed = 2;
+local Root = LawnApp.UIRoot;
+LawnApp.OnUpdate:Connect(function ()
+    local Children = LawnApp.UIRoot:GetChildren();
+    for _, Element in pairs(Children) do
+        Element.InitPos = Element.InitPos or Element.Position;
+        Element.Position = Element.Position + Vector2(math.random(-2, 2), math.random(-2, 2));
+    end;
+end)
